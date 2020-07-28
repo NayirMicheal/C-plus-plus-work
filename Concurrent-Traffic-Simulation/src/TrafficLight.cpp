2ec577882774459fa84ea4a03ef09f38346d2d31
Nayir Michael <nayir.mich.shaw.rizk@hotmail.com>
2020-07-28 12:15:43 +0200

    Update TrafficLight.cpp

--- Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -25,6 +25,7 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
         std::lock_guard<std::mutex> uLock(mtx);
+        _queue.clear(); //enhances the performance of the subsidiary traffic lights
         _queue.emplace_front(std::move(msg));
      
         cond_var.notify_one();
